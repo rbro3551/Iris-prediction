e79e38c34dd93d95e0aef79efb47ff4611fc0cfc
Riley Brookins <rileybrookins2@gmail.com>
2021-11-27 16:49:43 -0600

    added program file

---
+++ main.py
@@ -0,0 +1,99 @@
+# Check the versions of libraries
+
+# Python version
+import sys
+
+print('Python: {}'.format(sys.version))
+# scipy
+import scipy
+
+print('scipy: {}'.format(scipy.__version__))
+# numpy
+import numpy
+
+print('numpy: {}'.format(numpy.__version__))
+# matplotlib
+import matplotlib
+
+print('matplotlib: {}'.format(matplotlib.__version__))
+# pandas
+import pandas
+
+print('pandas: {}'.format(pandas.__version__))
+# scikit-learn
+import sklearn
+
+print('sklearn: {}'.format(sklearn.__version__))
+
+# Load libraries
+from pandas import read_csv
+from pandas.plotting import scatter_matrix
+from matplotlib import pyplot
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+# Load dataset
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = read_csv(url, names=names)
+
+print(dataset.shape)  # Function of pandas to show the rows and columns of the set
+
+print(dataset.head(20))  # Another pandas function to print first 20 rows of data
+
+print(dataset.describe())  # Summary of each attribute
+
+print(dataset.groupby('class').size())  # distribution by class
+
+dataset.plot(kind='box', subplots=True, layout=(2, 2), sharex=False, sharey=False)  # Show box plots for the data
+pyplot.show()
+
+# Creating Histograms
+dataset.hist()
+pyplot.show()
+
+scatter_matrix(dataset)  # Scatter plot
+pyplot.show()
+
+# Split-out validation dataset
+array = dataset.values
+X = array[:, :4]
+y = array[:, 4]
+
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.20, random_state=1)
+
+# Add algorithms to list to be checked
+models = [('LR', LogisticRegression(solver='liblinear', multi_class='ovr')), ('LDA', LinearDiscriminantAnalysis()),
+          ('KNN', KNeighborsClassifier()), ('CART', DecisionTreeClassifier()), ('NB', GaussianNB()),
+          ('SVM', SVC(gamma='auto'))]
+# evaluating the models
+results = []
+names = []
+for name, model in models:
+    kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+    cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
+    results.append(cv_results)
+    names.append(name)
+    print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
+
+# Support Vector Machines has the highest accuracy
+
+# Make predictions on validation dataset
+model = SVC(gamma='auto')
+model.fit(X_train, Y_train)
+predictions = model.predict(X_validation)
+
+# Evaluate predictions
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
